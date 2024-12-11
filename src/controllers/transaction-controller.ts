f27f1226c8423ca67f17b5925768f7e7d5f82597
Carlos <carlosmatiellisantos@gmail.com>
2024-12-11 11:59:10 -0300

    almost

--- src/controllers/transaction-controller.ts
+++ src/controllers/transaction-controller.ts
@@ -4,6 +4,19 @@ import transactionServices from "../services/transaction-service";
 import { transactionParams } from "../protocols";
 import { errorMessages } from "../utils/error-utils";
 
+export async function getTransaction(req: Request, res: Response) {
+    try {
+    const allTransactions = await transactionServices.getTransaction();
+    res.status(httpStatus.OK).send(allTransactions);
+    } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    return res
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .send("Não foi possível realizar o Get.")
+    }
+}
+
 export async function createTransaction(req: Request, res: Response) {
     const { id,description, value, type, userId } = req.body as transactionParams;
 
--- src/services/transaction-service.ts
+++ src/services/transaction-service.ts
@@ -1,3 +1,5 @@
+import { prisma } from "../config";
+import { notFoundError } from "../errors/not-found-error";
 import { transactionParams } from "../protocols";
 import transactionRepository from "../repositories/transaction-repository";
 
@@ -5,8 +7,17 @@ async function createTransaction(data: transactionParams) {
     return transactionRepository.createTransaction(data)
 }
 
+async function getTransaction() {
+    const list = await prisma.transaction.findMany();
+    
+    if (!list || !list[0]) throw notFoundError();
+
+    return list;
+}
+
 const transactionServices = {
     createTransaction,
+    getTransaction,
 }
 
 export default transactionServices;
