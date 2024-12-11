06e9d7f132a2f5f07ca83984440f600b43457bd9
Carlos <carlosmatiellisantos@gmail.com>
2024-12-11 11:59:26 -0300

    carlos

--- src/routers/transaction-router.ts
+++ src/routers/transaction-router.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 import { validateBody } from "../middlewares";
 import { transactionSchema} from "../schemas"
-import { createTransaction } from "../controllers";
+import { createTransaction, getTransaction } from "../controllers";
 
 const transactionRouter = Router();
 
 transactionRouter.post("/", validateBody(transactionSchema), createTransaction);
+transactionRouter.get("/", getTransaction);
 
 export { transactionRouter };
 
